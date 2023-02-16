61ba785597c5e1a75d2834bcd652fb387fb95c57
Binh <hoangquocbinh0411@gmail.com>
2023-02-16 19:09:29 +0700

    fix: image display

--- dice/script.js
+++ dice/script.js
@@ -6,50 +6,50 @@ function dice(){
     let y = Math.floor(Math.random() * (6 - 1 + 1) + 1);
     switch(x){
         case 1: 
-            firstResult.src = '/images/dice1.png';
+            firstResult.src = 'images/dice1.png';
         break;
         case 2: 
-            firstResult.src = '/images/dice2.png';
+            firstResult.src = 'images/dice2.png';
         break;
 
         case 3: 
-            firstResult.src = '/images/dice3.png';
+            firstResult.src = 'images/dice3.png';
         break;
         case 4: 
-            firstResult.src = '/images/dice4.png';
+            firstResult.src = 'images/dice4.png';
         break;
 
         case 5: 
-            firstResult.src = '/images/dice5.png';
+            firstResult.src = 'images/dice5.png';
         break;
         
         case 6: 
-            firstResult.src = '/images/dice6.png';
+            firstResult.src = 'images/dice6.png';
         break;
         }
     
     switch(y){
         case 1: 
-            secondResult.src = '/images/dice1.png';
+            secondResult.src = 'images/dice1.png';
             break;
         case 2: 
-            secondResult.src = '/images/dice2.png';
+            secondResult.src = 'images/dice2.png';
             break;
     
         case 3: 
-            secondResult.src = '/images/dice3.png';
+            secondResult.src = 'images/dice3.png';
             break;
     
         case 4: 
-            secondResult.src = '/images/dice4.png';
+            secondResult.src = 'images/dice4.png';
             break;
     
         case 5: 
-            secondResult.src = '/images/dice5.png';
+            secondResult.src = 'images/dice5.png';
             break;
             
         case 6: 
-            secondResult.src = '/images/dice6.png';
+            secondResult.src = 'images/dice6.png';
             break;
     }
         let winner = document.querySelector('h1');
