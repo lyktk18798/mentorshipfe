81fa08156381cb96fbba0e5d2227258796f4d181
sdhieu <sdhieu143@gmail.com>
2023-02-17 16:27:46 +0700

    remove history folder

--- .history/drumkit/index_20230213113619.js
+++
@@ -1,8 +0,0 @@
-let drum = document.querySelectorAll('.drum');
-for(let i = 0; i<drum.length; i++){
-    drum[i].addEventListener("click", function(){
-        let btn = drum.innerHtml;
-        makeSound(btn);
-        btnAnimation(btn);
-    })
-}
