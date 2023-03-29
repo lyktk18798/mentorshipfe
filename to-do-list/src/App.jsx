6baa6ff2c914ccff6495e1d932c56cb7f088a841
sdhieu <sdhieu143@gmail.com>
2023-03-29 15:16:23 +0700

    feat: add To Do List

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ToDoList from "./components/ToDoList";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <ToDoList />
+    </div>
+  );
+};
+
+export default App;
