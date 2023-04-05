6564bd6ebebd9776e6b72a419530443bb472625e
Binh <hoangquocbinh0411@gmail.com>
2023-04-05 17:49:47 +0700

    refactor: restructure code

--- keeper-app/src/Note.jsx
+++ keeper-app/src/Note.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import notes from "./notes";
 
-const EachNote = (props) => {
-    return (
-        <div class="note">
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
-        </div>
-    )
-}
-
-const CreateNote = (item) => {
-    return(
-        <EachNote 
-            title={item.title}
-            content={item.content}
-        />
-    )
-}
 const Note = () => {
-    return (
-        notes.map(item => CreateNote(item))
-    )
-}
+  return notes
+    ? notes.map((item) => {
+        return (
+          <div class="note">
+            <h1>{item.title}</h1>
+            <p>{item.content}</p>
+          </div>
+        );
+      })
+    : null;
+};
 
-export default Note
+export default Note;
