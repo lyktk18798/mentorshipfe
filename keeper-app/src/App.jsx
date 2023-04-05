fea75b7d11434955b1b1ceb9e26beca4ae306a17
sdhieu <sdhieu143@gmail.com>
2023-04-05 18:28:20 +0700

    feat: add notes list

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
+import notes from "./notes";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
-      <Note></Note>
+      {notes.map((item) => (
+        <Note keu={item.id} title={item.title} content={item.content}></Note>
+      ))}
       <Footer></Footer>
     </div>
   );
