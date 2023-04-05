089e471c5cdcac78c258ac134c06a1269fd8b6d8
duongng59 <hoangduong1998@gmail.com>
2023-04-05 13:04:39 +0700

    feat: keeper app task 1

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>for title</h1>
+      <p> for content</p>
+    </div>
+  );
+};
+export default Note;
