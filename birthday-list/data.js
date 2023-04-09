ea2b149391a1a9b901ab199294814f15d2865f26
Binh <hoangquocbinh0411@gmail.com>
2023-04-09 15:31:23 +0700

    feat: initailize birthday list

---
+++ birthday-list/data.js
@@ -0,0 +1,75 @@
+import bee from "./src/img/bee.png";
+import cow from "./src/img/cow.png";
+import fox from "./src/img/fox.png";
+import hen from "./src/img/hen.png";
+import koala from "./src/img/koala.png";
+import penguin from "./src/img/penguin.png";
+import sheep from "./src/img/sheep.png";
+import snail from "./src/img/snail.png";
+import snake from "./src/img/snake.png";
+import whale from "./src/img/whale.png";
+
+const animalList = [
+  {
+    id: 1,
+    name: "Bee",
+    age: "6 years",
+    src: bee,
+  },
+  {
+    id: 2,
+    name: "Cow",
+    age: "10 years",
+    src: cow,
+  },
+  {
+    id: 3,
+    name: "Fox",
+    age: "2 years",
+    src: fox,
+  },
+  {
+    id: 4,
+    name: "Hen",
+    age: "9 years",
+    src: hen,
+  },
+  {
+    id: 5,
+    name: "Koala",
+    age: "12 years",
+    src: koala,
+  },
+  {
+    id: 6,
+    name: "Penguin",
+    age: "1 year",
+    src: penguin,
+  },
+  {
+    id: 7,
+    name: "Sheep",
+    age: "6 years",
+    src: sheep,
+  },
+  {
+    id: 8,
+    name: "Snail",
+    age: "4 years",
+    src: snail,
+  },
+  {
+    id: 9,
+    name: "Snake",
+    age: "10 years",
+    src: snake,
+  },
+  {
+    id: 10,
+    name: "Whale",
+    age: "12 years",
+    src: whale,
+  },
+];
+
+export default animalList;
---
+++ birthday-list/src/component/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "./List";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h2 className="text-center">Birthdays Today</h2>
+      <List />
+    </div>
+  );
+}
+
+export default App;
