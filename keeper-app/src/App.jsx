f4e4c24164cee4f637a2a736a5307fabc4bb98ca
sdhieu <sdhieu143@gmail.com>
2023-04-13 19:04:42 +0700

    feat: Keeper App Last

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
-import notes from "./notes";
+// import notes from "./notes";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
-      {notes.map((item) => (
-        <Note keu={item.id} title={item.title} content={item.content}></Note>
-      ))}
+      <Note />
       <Footer></Footer>
     </div>
   );
